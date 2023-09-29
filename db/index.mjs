dbacdbc1615e80392f25b2c3e7f41c502797afd9
Glendon Philipp Baculio <gpbaculio@gmail.com>
2023-09-30 03:11:38 +0800

    initialize db with nodemon for dev

---
+++ db/index.mjs
@@ -0,0 +1,10 @@
+import express from "express";
+import { createHandler } from "graphql-http/lib/use/express";
+import schema from "./schema.mjs";
+
+const app = express();
+// use postman as replacement for graphiql
+app.all("/graphql", createHandler({ schema }));
+
+app.listen({ port: 4000 });
+console.log("Listening to port 4000");
