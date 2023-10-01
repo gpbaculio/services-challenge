413dc796f5907d42d550c61e25596a46689635bd
Glendon Philipp Baculio <gpbaculio@gmail.com>
2023-10-01 12:04:10 +0800

    service request field with pagination

---
+++ db/query/types/ServiceRequestType.mjs
@@ -0,0 +1,67 @@
+import { GraphQLObjectType, GraphQLString } from "graphql";
+import {
+  connectionDefinitions,
+  fromGlobalId,
+  globalIdField,
+  nodeDefinitions,
+} from "graphql-relay";
+import { PrismaClient } from "@prisma/client";
+
+const prisma = new PrismaClient();
+
+export const { nodeInterface, nodeField } = nodeDefinitions(
+  async (globalId) => {
+    const { type, id } = fromGlobalId(globalId);
+    if (type === "ServiceRequest") {
+      return await prisma.serviceRequest.findUnique({ where: { id } });
+    } else {
+      return null;
+    }
+  },
+  (obj) => {
+    // definitely a service request if valid
+    if (obj?.servicetype) {
+      return ServiceRequestType;
+    } else {
+      return null;
+    }
+  }
+);
+
+const ServiceRequestType = new GraphQLObjectType({
+  name: "ServiceRequest",
+  fields: {
+    id: globalIdField("ServiceRequest"),
+    customerName: {
+      type: GraphQLString,
+      resolve: ({ customerName }) => customerName,
+    },
+    serviceType: {
+      type: GraphQLString,
+      resolve: ({ serviceType }) => serviceType,
+    },
+    status: {
+      type: GraphQLString,
+      resolve: ({ status }) => status,
+    },
+    scheduledDate: {
+      type: GraphQLString,
+      resolve: ({ scheduledDate }) => scheduledDate,
+    },
+    contactInfo: {
+      type: GraphQLString,
+      resolve: ({ contactInfo }) => contactInfo,
+    },
+  },
+  interfaces: [nodeInterface],
+});
+
+export const {
+  connectionType: ServiceRequestConnection,
+  edgeType: GraphQLServiceRequestEdge,
+} = connectionDefinitions({
+  name: "ServiceRequest",
+  nodeType: ServiceRequestType,
+});
+
+export default ServiceRequestType;
