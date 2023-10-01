237b91b20df2998fcb4ff6cf369718eae11f1a0d
Glendon Philipp Baculio <gpbaculio@gmail.com>
2023-10-01 12:04:18 +0800

    viewer field

---
+++ db/query/types/Viewer.mjs
@@ -0,0 +1,37 @@
+import { GraphQLObjectType } from "graphql";
+import {
+  connectionArgs,
+  connectionFromArray,
+  globalIdField,
+} from "graphql-relay";
+import { PrismaClient } from "@prisma/client";
+
+import {
+  ServiceRequestConnection,
+  nodeInterface,
+} from "./ServiceRequestType.mjs";
+
+const prisma = new PrismaClient();
+
+const GraphQLViewerType = new GraphQLObjectType({
+  name: "Viewer",
+  interfaces: [nodeInterface],
+  fields: () => ({
+    id: globalIdField("Viewer"),
+    serviceRequests: {
+      type: ServiceRequestConnection,
+      args: { ...connectionArgs },
+      resolve: async (_, { ...args }) => {
+        console.log("args", args);
+        const serviceRequests = await prisma.serviceRequest({
+          skip: args?.after || undefined,
+          first: args?.first || undefined,
+        });
+
+        return connectionFromArray(serviceRequests, args);
+      },
+    },
+  }),
+});
+
+export default GraphQLViewerType;
