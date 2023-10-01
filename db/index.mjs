837491311c37d9d8e1ec0c96deb82bb620338c6d
Glendon Philipp Baculio <gpbaculio@gmail.com>
2023-10-02 04:08:12 +0800

    fix cors

--- db/index.mjs
+++ db/index.mjs
@@ -1,9 +1,12 @@
 import express from "express";
 import { createHandler } from "graphql-http/lib/use/express";
+import cors from "cors";
 
 import schema from "./schema.mjs";
 
 const app = express();
+
+app.use(cors());
 // use postman as replacement for graphiql
 app.all("/graphql", createHandler({ schema }));
 
