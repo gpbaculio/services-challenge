bc33f68d16de02a5db95cee3f034f27436abc82a
Glendon Philipp Baculio <gpbaculio@gmail.com>
2023-10-07 07:40:58 +0800

    form on modal to create user

--- services-web/src/app/super-admin/page.tsx
+++ services-web/src/app/super-admin/page.tsx
@@ -1,12 +1,38 @@
 "use client";
 
-import React, { useState } from "react";
+import React, { LegacyRef, useState } from "react";
 
 import Header from "@/components/Header";
 import Modal from "@/components/Modal";
+import { useForm } from "react-hook-form";
+import { yupResolver } from "@hookform/resolvers/yup";
+import * as yup from "yup";
+
+const schema = yup.object().shape({
+  name: yup.string().required(),
+  email: yup.string().email().required(),
+  password: yup.string().required(),
+});
+
+type FormValues = {
+  name: string;
+  email: string;
+  password: string;
+};
 
 function Page() {
   const [showModal, setShowModal] = useState(false);
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm<FormValues>({
+    resolver: yupResolver(schema),
+  });
+
+  function onSubmit(data: FormValues) {
+    console.log("data ", data);
+  }
 
   return (
     <div className="min-h-screen w-full bg-blue-100 ">
@@ -30,7 +56,79 @@ function Page() {
           setShowModal(false);
         }}
       >
-        <h1>Test</h1>
+        <form onSubmit={handleSubmit(onSubmit)}>
+          <div className="mb-4">
+            <label
+              className="block text-gray-700 font-bold mb-2"
+              htmlFor="name"
+            >
+              Name
+            </label>
+            <input
+              className={`appearance-none border ${
+                errors.name ? "border-red-500" : "border-gray-200"
+              } rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline`}
+              type="text"
+              id="name"
+              {...register("name")}
+            />
+            {errors.name && (
+              <p className="text-red-500 text-xs italic">
+                {errors.name.message}
+              </p>
+            )}
+          </div>
+          <div className="mb-4">
+            <label
+              className="block text-gray-700 font-bold mb-2"
+              htmlFor="email"
+            >
+              Email
+            </label>
+            <input
+              className={`appearance-none border ${
+                errors.email ? "border-red-500" : "border-gray-200"
+              } rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline`}
+              type="email"
+              id="email"
+              {...register("email")}
+            />
+            {errors.email && (
+              <p className="text-red-500 text-xs italic">
+                {errors.email.message}
+              </p>
+            )}
+          </div>
+          <div className="mb-4">
+            <label
+              className="block text-gray-700 font-bold mb-2"
+              htmlFor="password"
+            >
+              Password
+            </label>
+            <input
+              className={`appearance-none border ${
+                errors.password ? "border-red-500" : "border-gray-200"
+              } rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline`}
+              type="password"
+              id="password"
+              {...register("password")}
+            />
+            {errors.password && (
+              <p className="text-red-500 text-xs italic">
+                {errors.password.message}
+              </p>
+            )}
+          </div>
+          <div className="flex items-center justify-between">
+            <button
+              className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline"
+              type="submit"
+            >
+              Create User
+            </button>
+          </div>
+        </form>
       </Modal>
     </div>
   );
