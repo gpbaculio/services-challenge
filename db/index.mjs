8a91125e389f3cdaf56e47afbbb542aed79ac025
Glendon Philipp Baculio <gpbaculio@gmail.com>
2023-10-07 04:11:26 +0800

    get user from token

--- db/index.mjs
+++ db/index.mjs
@@ -4,13 +4,22 @@ import { createHandler } from "graphql-http/lib/use/express";
 import cors from "cors";
 
 import schema from "./schema.mjs";
+import { getUser } from "./auth.mjs";
 
 const app = express();
 
 app.use(cors());
 // use postman as replacement for graphiql
-app.all("/graphql", (req, res) =>
-  createHandler({ schema, context: { req, res } })
+app.all(
+  "/graphql",
+  createHandler({
+    schema,
+    context: async (req) => {
+      const { user } = await getUser(req?.headers?.get("Authorization"));
+
+      return { user };
+    },
+  })
 );
 
 app.listen({ port: 4000 });
