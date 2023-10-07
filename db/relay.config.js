6eb9eb8b577cece5fc82014d1d77ceb2d9cb745c
Glendon Philipp Baculio <gpbaculio@gmail.com>
2023-10-08 06:56:05 +0800

    Create relay.config.js

---
+++ db/relay.config.js
@@ -0,0 +1,7 @@
+module.exports = {
+  src: "./src",
+  schema: "./schema.graphql",
+  language: "typescript",
+  excludes: ["**/.next/**", "**/node_modules/**", "**/schema/**"],
+  artifactDirectory: "./__generated__",
+};
