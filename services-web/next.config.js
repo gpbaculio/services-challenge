b617f328a6ea819fd24512fe0ce7e7aa38caa30a
Glendon Philipp Baculio <gpbaculio@gmail.com>
2023-10-08 06:56:17 +0800

    Update next.config.js

--- services-web/next.config.js
+++ services-web/next.config.js
@@ -1,11 +1,16 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  reactStrictMode: true,
+  swcMinify: true,
   compiler: {
     relay: {
       src: "./",
       language: "typescript",
       artifactDirectory: "__generated__",
     },
+  },
+  experimental: {
+    serverActions: true,
   },
 };
 
