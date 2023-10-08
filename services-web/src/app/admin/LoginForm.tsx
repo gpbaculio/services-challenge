58c307c02c74afcb0c40cfea3c8e2a5842d6ff62
Glendon Philipp Baculio <gpbaculio@gmail.com>
2023-10-09 02:27:15 +0800

    simplify

---
+++ services-web/src/app/admin/LoginForm.tsx
@@ -0,0 +1,80 @@
+import React from "react";
+
+import { useForm } from "react-hook-form";
+
+import { SubmitButton } from "../super-admin/page";
+
+import useLoginYupResolver from "./useLoginYupResolver";
+import useLogin from "./useLogin";
+
+type PageFormValues = {
+  email: string;
+  password: string;
+};
+
+function LoginForm() {
+  const resolver = useLoginYupResolver();
+  const { login, loginSuccess, isLoading, loginFailure } = useLogin();
+
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm<PageFormValues>({ resolver });
+
+  const onSubmit = (input: PageFormValues) => {
+    login(input);
+  };
+
+  return (
+    <fieldset disabled={isLoading}>
+      <form
+        onSubmit={handleSubmit(onSubmit)}
+        className="bg-white rounded shadow p-4 w-[400px] flex flex-col"
+      >
+        <div className="mb-4">
+          <label className="block text-gray-700 font-bold mb-2" htmlFor="email">
+            Email
+          </label>
+          <input
+            className={`appearance-none border ${
+              errors.email ? "border-red-500" : "border-gray-200"
+            } rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline`}
+            type="email"
+            id="email"
+            {...register("email")}
+          />
+          {errors.email && (
+            <p className="text-red-500 text-xs italic">
+              {errors.email.message}
+            </p>
+          )}
+        </div>
+        <div className="mb-4">
+          <label
+            className="block text-gray-700 font-bold mb-2"
+            htmlFor="password"
+          >
+            Password
+          </label>
+          <input
+            className={`appearance-none border ${
+              errors.password ? "border-red-500" : "border-gray-200"
+            } rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline`}
+            type="password"
+            id="password"
+            {...register("password")}
+          />
+          {errors.password && (
+            <p className="text-red-500 text-xs italic">
+              {errors.password.message}
+            </p>
+          )}
+        </div>
+        <SubmitButton isLoading={isLoading} label="Login" />
+      </form>
+    </fieldset>
+  );
+}
+
+export default LoginForm;
--- services-web/src/app/admin/useLogin.ts
+++ services-web/src/app/admin/useLogin.ts
@@ -1,9 +1,14 @@
 "use client";
 
+import { useCallback, useState } from "react";
+
 import { graphql } from "relay-runtime";
 import { useMutation } from "react-relay";
 
-import { useLoginMutation } from "../../../__generated__/useLoginMutation.graphql";
+import {
+  UserLoginInput,
+  useLoginMutation,
+} from "../../../__generated__/useLoginMutation.graphql";
 
 const graphQLMutation = graphql`
   mutation useLoginMutation($input: UserLoginInput!) {
@@ -16,5 +21,33 @@ const graphQLMutation = graphql`
 `;
 
 export default function useLogin() {
-  return useMutation<useLoginMutation>(graphQLMutation);
+  const [loginSuccess, setLoginSuccess] = useState(false);
+  const [loginFailure, setLoginFailure] = useState(false);
+
+  const [commit, isLoading] = useMutation<useLoginMutation>(graphQLMutation);
+
+  const login = useCallback(
+    (input: UserLoginInput) => {
+      commit({
+        variables: { input },
+        onCompleted({ loginUser }) {
+          if (loginUser?.token) {
+            localStorage.setItem("token", loginUser.token);
+            setLoginSuccess(true);
+          }
+          if (loginUser?.error) {
+            setLoginFailure(true);
+          }
+        },
+      });
+    },
+    [commit]
+  );
+
+  return {
+    login,
+    isLoading,
+    loginSuccess,
+    loginFailure,
+  };
 }
