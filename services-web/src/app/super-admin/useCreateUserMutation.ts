4245cb7916104018bfef91bab91c4a0290925284
Glendon Philipp Baculio <gpbaculio@gmail.com>
2023-10-08 12:44:34 +0800

    Update useCreateUserMutation.ts

--- services-web/src/app/super-admin/useCreateUserMutation.ts
+++ services-web/src/app/super-admin/useCreateUserMutation.ts
@@ -1,6 +1,8 @@
 "use client";
+
 import { graphql } from "relay-runtime";
 import { useMutation } from "react-relay";
+
 import { useCreateUserMutation } from "../../../__generated__/useCreateUserMutation.graphql";
 
 const mutation = graphql`
